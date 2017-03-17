5a9d727c7a41dcf2f94d9e97938711978c622e29
Boris <borya.ivchenko@mail.ru>
2017-03-17 19:18:29 +0000

    use cookie

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -1,17 +1,4 @@
-var app = angular.module('nightlife', ['ngResource', 'ngRoute']);
-
-app.factory('loggedInterceptor', ['$rootScope', '$q', '$location', function($rootScope, $q, $location) {
-  return { 
-    responseError: function(response) { 
-      if (response.status === 401){
-        $location.url('/login');
-        return $q.reject(response);
-      }
-      else
-        return $q.reject(response); 
-    } 
-  };
-}]);
+var app = angular.module('nightlife', ['ngResource', 'ngRoute', 'ngCookies']);
 
 app.config(['$routeProvider', '$locationProvider', '$httpProvider', function($routeProvider, $locationProvider, $httpProvider) {
   var checkLoggedin = function($q, $http, $location, $rootScope){
@@ -30,26 +17,39 @@ app.config(['$routeProvider', '$locationProvider', '$httpProvider', function($ro
   };
   $routeProvider
     .when('/', { controller: 'MainCtrl', resolve: { loggedin: checkLoggedin }})
-    
-  $routeProvider.otherwise({ redirectTo: "/" });
+    .otherwise({ redirectTo: "/" });
   $locationProvider.html5Mode({ enabled: true, requireBase: false});
-  $httpProvider.interceptors.push('loggedInterceptor');
 }]);
 
 
-app.controller('MainCtrl', ['$scope', '$http', '$route', function($scope, $http, $route){
+app.controller('MainCtrl', ['$scope', '$http', '$route', '$cookies', function($scope, $http, $route, $cookies){
   $scope.items = [];
   var query;
   
-  $scope.find = function() {
-    query = $scope.query;
-    $http.get('/find/' + query).then(function(response){
+  function search(q) {
+    $http.get('/find/' + q).then(function(response){
       $scope.items = response.data.bars;
-      console.log(response.data)
       $scope.items.businesses.forEach(function(item, i){
         item.count = response.data.counts[i];
       })
+      console.log(response.data)
     })
+  }
+  
+  var cookie = $cookies.getObject('savedQuery');
+  if(cookie) {
+    $scope.query = query = cookie;
+    search(query);
+    console.log(cookie, $scope.items)
+  }
+  
+  $scope.find = function() {
+    query = $scope.query;
+    var today = new Date();
+    var expiresValue = new Date(today);
+    expiresValue.setMinutes(today.getMinutes() + 5); 
+    $cookies.putObject('savedQuery', query, { 'expires': expiresValue });
+    search(query);
   }
   
   $scope.go = function (eventId, index) {
@@ -59,4 +59,6 @@ app.controller('MainCtrl', ['$scope', '$http', '$route', function($scope, $http,
       $route.reload();
     })
   }
+  
+  $scope.clicked = 30;
 }]);
